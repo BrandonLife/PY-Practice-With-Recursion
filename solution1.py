4fb7d9a6ceac6587bd86d2577953bbeb7af7a396
Brandon Life <blife1119@gmail.com>
2023-10-18 19:59:58 -0400

    finished solution

--- solution1.py
+++ solution1.py
@@ -1,1 +1,13 @@
 # Write code for algorithm 1 below
+
+
+def recursion(num):
+    if num == 0:
+        return
+    else:
+        print(num)
+        return recursion(num - 1)
+
+
+n = 4
+print(recursion(n))
